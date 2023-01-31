3cd13399f91a0e0f661d550c049197958a31bc1b
Soundryacedcoss <soundryarani@cedcoss.com>
2023-01-31 16:44:11 +0530

    Done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@cedcommerce/ounce-ui";
+import { Login } from "./Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
